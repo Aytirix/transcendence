f7b6a8789b6db4e80513de998d0e7d1abb8bd47c
Théo <aytirix@hotmail.com>
2025-04-18 17:36:01 +0200

    Ajout du refresh auto pour le front

--- srcs/container/frontend/srcs/vite.config.ts
+++ srcs/container/frontend/srcs/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		hmr: false
-	  }
+		hmr: {
+			protocol: 'wss',
+			host: 'localhost',
+			port: 3000,
+		}
+	}
 });
