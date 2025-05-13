533013871bd06f445b9048cd62cb4136c2ee3116
irongab06 <gabrielcavalier@msn.com>
2025-05-13 23:42:49 +0200

    ia fonctionnel sans actualisation et deco

--- srcs/container/backend/srcs/src/WebSocket/pongGame/game/Game.ts
+++ srcs/container/backend/srcs/src/WebSocket/pongGame/game/Game.ts
@@ -10,25 +10,31 @@ export class Game {
 		private player1: Paddle,
 		private player2: Paddle,
 		private frameRate: number = 0,
+		private detectionPaddle: boolean = false,
 		private readonly width: number = 800,
 		private readonly height: number = 600,
-		private status: "PLAYING" | "WAITING" | "EXIT" = "PLAYING",
+		private status: "PLAYING" | "KICKOFF" | "EXIT" = "PLAYING",
 		private jsonWebsocket: string = ""
 	) {}
 	start(): void{
 		const idInterval = setInterval(() => {
 			if (this.update())
 				clearInterval(idInterval);
-		}, 1000 / 60) // 60
-		// setInterval(() => {
-		// 	this.player2.getAi().getStateFromGame(this.ball, this.player2);
-		// 	this.player2.getAi().chooseAction();
-		// 	this.player2.getAi().updateQtable();
-		// }, 1000)
-	};
+			if (this.player1.getPlayerInfos().mode === "Solo") {
+				if (this.detectionPaddle === true) {
+					this.detectionPaddle = false;
+					this.frameRate = 0;
+					this.player2.getAi().getStateFromGame(this.ball, this.player1, this.player2);
+					this.player2.getAi().updateQtable();
+					this.player2.getAi().chooseAction();
+					this.player2.getAi().setReward(0);
+				}
+			}
+		}, 1000 / 60)
+	}
 	update(): boolean {
 		this.frameRate++;
-		if (this.getStatus() === "WAITING") { return false }
+		if (this.getStatus() === "KICKOFF") { return false }
 		this.ball.move();
 		this.detectionCollision();
 		this.jsonWebsocket = JSON.stringify({
@@ -66,14 +72,13 @@ export class Game {
 			}
 		else if (this.player1.getPlayerInfos().mode === "SameKeyboard")
 			this.player1.getPlayerInfos().socket.send(this.jsonWebsocket);
-		// else if (this.player1.getPlayerInfos().mode === "Solo") {
-		// 	this.player1.pos_y = this.ball.pos_y;
-		// 	this.player1.getPlayerInfos().socket.send(this.jsonWebsocket);
-		// 	if (this.frameRate < this.player2.getAi().getLimitRate())
-		// 		this.player2.move(this.player2.getAi().getAction());
-		// 	else
-		// 		this.frameRate = 0;
-		// }
+		else if (this.player1.getPlayerInfos().mode === "Solo") {
+			this.player1.getPlayerInfos().socket.send(this.jsonWebsocket);
+			if (this.frameRate < this.player2.getAi().getLimitRate()){
+				
+				this.player2.move(this.player2.getAi().getAction());
+			}
+		}
 		if (this.checkScore(this.player1, this.player2)) {
 			if (this.player1.getPlayerInfos().mode === "Multi"
 			&& this.player2.getPlayerInfos().mode === "Multi") {
@@ -87,14 +92,6 @@ export class Game {
 				this.player1.getPlayerInfos().socket.send(JSON.stringify({type: "EXIT"}));
 				handleFinish(this.player1.getPlayerInfos());
 				this.resetDisplay("SameKeyboard");
-
-				// const filePath = join(__dirname, 'pongAi', 'fileJson', 'qLearning.json');
-				// writeFile(filePath, JSON.stringify(this.player2.getAi().qTable, null, 2), (err) => {
-				// 	if (err)
-				// 		console.error("❌ Erreur d’écriture fichier IA :", err);
-				// 	else
-				// 		console.log("✅ IA sauvegardée avec succès !");
-				// });
 			}
 			return (true);
 
@@ -112,15 +109,6 @@ export class Game {
 				this.player1.getPlayerInfos().socket.send(JSON.stringify({type: "EXIT"}));
 				handleFinish(this.player1.getPlayerInfos());
 				this.resetDisplay("SameKeyboard");
-
-				// const filePath = join(__dirname, 'pongAi', 'fileJson', 'qLearning.json');
-
-				// writeFile(filePath, JSON.stringify(this.player2.getAi().qTable, null, 2), (err) => {
-				// 	if (err)
-				// 		console.error("❌ Erreur d’écriture fichier IA :", err);
-				// 	else
-				// 		console.log("✅ IA sauvegardée avec succès !");
-				// });
 			}
 			return (true);
 		}
@@ -129,40 +117,61 @@ export class Game {
 	detectionCollision(): void {
 		if (this.player1.isCollidingWithBall(this.ball)) {
 			this.ball.d_x = -1;
+			if (this.ball.speed <= 12.5)
+				this.ball.speed += 0.5;
 			this.player1.zoneEffect(this.ball);
+			console.log("ball cote player1 :",this.ball.pos_y)
+			if (this.player1.getPlayerInfos().mode === "Solo") {
+				this.player2.getAi().updateReward(4);
+				this.player2.getAi().getReboundBall(this.ball, this.player2, this.player1);
+				this.detectionPaddle = true;
+			}
 		}
 		else if ((this.ball.pos_x + this.ball.radius) <= 0) {
 			console.log("player 1 marque ")
 			this.player1.setScore();
-			// this.player2.getAi().updateReward(2);
-			this.serviceBall(0, this.ball);
+			this.serviceBall(0, this.ball, this.player1, this.player2);
 		}
 		else if (this.player2.isCollidingWithBall(this.ball)) {
 			this.ball.d_x = 1;
-			// this.player2.getAi().updateReward(3);
+			if (this.ball.speed <= 12.5)
+				this.ball.speed += 0.5;
 			this.player2.zoneEffect(this.ball);
+			console.log("ball cote player2 :",this.ball.pos_y)
+			if (this.player1.getPlayerInfos().mode === "Solo") {
+				this.player2.getAi().updateReward(3);
+				this.player2.getAi().getReboundBall(this.ball, this.player2, this.player1);
+				this.detectionPaddle = true;
+			}
 		}
 		else if ((this.ball.pos_x - this.ball.radius) >= this.width ){
 			console.log("player 2 marque ")
 			this.player2.setScore();
-			// this.player2.getAi().updateReward(1);
-			this.serviceBall(1, this.ball);
+			this.serviceBall(1, this.ball, this.player1, this.player2);
 		}
 		if ((this.ball.pos_y - this.ball.radius)<= 0) {
 			this.ball.d_y = 1;
+			if (this.player1.getPlayerInfos().mode === "Solo"&& this.ball.d_x > 0) {
+				this.player2.getAi().updateReward(5);
+			}
 		}
 		else if ((this.ball.pos_y  + this.ball.radius)>= this.height) {
 			this.ball.d_y = -1;
+			if (this.player1.getPlayerInfos().mode === "Solo"&& this.ball.d_x > 0) {
+				this.player2.getAi().updateReward(5);
+			}
 		}
 	}
-	serviceBall(direction: number, ball: Ball) : void {
+	serviceBall(direction: number, ball: Ball, player1: Paddle, player2: Paddle) : void {
+		console.log(`vrai position de la ball en Y avant de sortir du terrain : ${this.ball.pos_y}`)
 		ball.pos_x = this.width / 2;
 		ball.pos_y = this.height / 2;
+		ball.speed = 7 //ici enlever
 		this.player1.pos_x = 780;
 		this.player1.pos_y = 250;
 		this.player2.pos_x = 20;
 		this.player2.pos_y = 250;
-		this.setStatus("WAITING");
+		this.setStatus("KICKOFF");
 		setTimeout(() => {
 			switch (direction) {
 				case 1 :
@@ -174,15 +183,22 @@ export class Game {
 					ball.d_y = 0;
 					break;
 			}
+			if (this.player1.getPlayerInfos().mode === "Solo") {
+				this.player2.getAi().updateReward(1);
+				this.player2.getAi().getReboundBall(this.ball, this.player2, this.player1);
+				this.detectionPaddle = true;
+			}
+		}, 2000); //2000
+		setTimeout(() => {
 			this.setStatus("PLAYING");
-		}, 0); //2000
+		}, 2000); //2000
 	}
 	checkScore(player1: Paddle, player2: Paddle) : boolean {
-		if (player1.getScore() == 5000) {
-			console.log("Winner is player 2")
+		if (player1.getScore() == 21) {
+			console.log("Winner is player 1")
 			return (true);
 		}
-		else if (player2.getScore() == 5000){
+		else if (player2.getScore() == 21){
 			console.log("Winner is player 2")
 			return (true);
 		}
@@ -201,5 +217,5 @@ export class Game {
 	getBall() : Ball {return (this.ball); }
 	getPlayer1() : Paddle {return (this.player1); }
 	getPlayer2() : Paddle {return (this.player2); }
-	setStatus(stat: "PLAYING" | "WAITING" | "EXIT") { this.status = stat; }
+	setStatus(stat: "PLAYING" | "KICKOFF" | "EXIT") { this.status = stat; }
 }
