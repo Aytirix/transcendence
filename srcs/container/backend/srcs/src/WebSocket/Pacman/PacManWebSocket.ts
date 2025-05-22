22473fc829f592f37f1c0fd8237607dc12dcf437
Théo <aytirix@hotmail.com>
2025-05-23 01:33:08 +0200

    okkye life pacman etc

--- srcs/container/backend/srcs/src/WebSocket/Pacman/PacManWebSocket.ts
+++ srcs/container/backend/srcs/src/WebSocket/Pacman/PacManWebSocket.ts
@@ -42,10 +42,10 @@ function createTestRoom(player: player, room: room): void {
 		gameId: null,
 		elo: 1000,
 	};
-	room.players.push(player2);
-	room.players.push(player3);
-	room.players.push(player4);
-	room.players.push(player5);
+	if (room.players.length < 5) room.players.push(player2);
+	if (room.players.length < 5) room.players.push(player3);
+	if (room.players.length < 5) room.players.push(player4);
+	if (room.players.length < 5) room.players.push(player5);
 }
 
 (async () => {
