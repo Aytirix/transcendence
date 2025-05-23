703f65d72a8d937f7fec9d968bf04ca323659903
K1000 <camille.gorin41@gmail.com>
2025-05-23 19:39:43 +0200

    enhance PacmanMap layout with header and responsive design adjustments

--- srcs/container/frontend/srcs/src/app/Pacman/theojeutmp/PacmanMap.tsx
+++ srcs/container/frontend/srcs/src/app/Pacman/theojeutmp/PacmanMap.tsx
@@ -4,8 +4,8 @@ import './PacmanMap.scss';
 import { state } from '../../types/pacmanTypes';
 import portalImg from '../../assets/img/pacman/portal.gif';	
 
-const CONTAINER_SIZE_WIDTH = 824; // Doit correspondre à la taille CSS
-const CONTAINER_SIZE_HEIGHT = 880; // Doit correspondre à la taille CSS
+const CONTAINER_SIZE_WIDTH = 824 -50 ; // Doit correspondre à la taille CSS
+const CONTAINER_SIZE_HEIGHT = 880 - 50; // Doit correspondre à la taille CSS
 
 export interface Player {
 	id: number;
@@ -101,33 +101,37 @@ const PacmanMap: React.FC<PacmanMapProps> = ({ state }) => {
 	  
 
 	return (
-		<div className="pacman-map-wrapper">
-			{/* <div className="pacman-map-header">
-				<h3 className="score">{players[0]?.username} : {players[0]?.score}</h3>
-				<h3 className="score">{players[1]?.username} : {players[1]?.score}</h3>
-				<h3 className="score">{players[2]?.username} : {players[2]?.score}</h3>
-				<h3 className="score">{players[3]?.username} : {players[3]?.score}</h3>
-				<h3 className="score">{players[4]?.username} : {players[4]?.score}</h3>
-			</div> */}
-			<div className="pacman-map-container" >
-				{/* 1. Dessiner la grille : un <div> par case */}
-				<div className='pacman-map'
-				style={{
-					position: 'absolute',
-					top: offsetY,
-					left: offsetX,
-					width: `${mapWidth}px`,
-					height: `${mapHeight}px`,
-					transform: `scale(${scale})`,
-					transformOrigin: 'top left'
-				  }}>
-					{grid.map((rowString, rowIndex) =>
-						rowString.split('').map((char, colIndex) => {
-							const tileClass = getTileClass(char, rowIndex, colIndex);
-							const topPx = rowIndex * tileSize;
-							const leftPx = colIndex * tileSize;
-							return (
-								<div
+		<>
+			<div className="header">
+				<h3 className="title">test</h3>
+			</div>
+			<div className="pacman-map-wrapper">
+				<div className='column-left'>
+					{/* <div className="pacman-map-header">
+						<h3 className="title">Pacman</h3>
+						<h3 className="title">Score</h3>
+						<h3 className="title">Vies</h3>
+						</div> */}
+				</div>
+				<div className="pacman-map-container" >
+					{/* 1. Dessiner la grille : un <div> par case */}
+					<div className='pacman-map'
+					style={{
+						position: 'absolute',
+						top: offsetY,
+						left: offsetX,
+						width: `${mapWidth}px`,
+						height: `${mapHeight}px`,
+						transform: `scale(${scale})`,
+						transformOrigin: 'top left'
+					}}>
+						{grid.map((rowString, rowIndex) =>
+							rowString.split('').map((char, colIndex) => {
+								const tileClass = getTileClass(char, rowIndex, colIndex);
+								const topPx = rowIndex * tileSize;
+								const leftPx = colIndex * tileSize;
+								return (
+									<div
 									key={`tile-${rowIndex}-${colIndex}`}
 									className={tileClass}
 									style={{
@@ -136,96 +140,107 @@ const PacmanMap: React.FC<PacmanMapProps> = ({ state }) => {
 										width: tileSize,
 										height: tileSize,
 									}}
-								>
-									{char === '-' && (
-										<div
+									>
+										{char === '-' && (
+											<div
 											className={`door ${
 												(grid[rowIndex]?.[colIndex - 1] === '#' && grid[rowIndex]?.[colIndex + 1] === '#')
-													? 'horizontal'
-													: (grid[rowIndex - 1]?.[colIndex] === '#' && grid[rowIndex + 1]?.[colIndex] === '#')
-													? 'vertical'
-													: ''
+												? 'horizontal'
+												: (grid[rowIndex - 1]?.[colIndex] === '#' && grid[rowIndex + 1]?.[colIndex] === '#')
+												? 'vertical'
+												: ''
 											}`}
-										/>
-									)}
-									{char === 'T' && <img src={portalImg} alt="portal" className="tunnel" />}
-									{/* 2. Dessiner les éléments de la grille */}
-									{char === '.' && <div className="dot" />}
-									{char === 'o' && <div className="big-dot" />}
-								</div>
-							);
-						})
-					)}
+											/>
+										)}
+										{char === 'T' && <img src={portalImg} alt="portal" className="tunnel" />}
+										{/* 2. Dessiner les éléments de la grille */}
+										{char === '.' && <div className="dot" />}
+										{char === 'o' && <div className="big-dot" />}
+									</div>
+								);
+							})
+						)}
 
-					{/* 2. Superposer les joueurs */}
-					{players.map(player => {
-						
-						const className =
+						{/* 2. Superposer les joueurs */}
+						{players.map(player => {
+							
+							const className =
 							player.character === 'P'
-								? 'player pacman'
-								: `player ghost ghost-${player.character}`;
-
-						// On soustrait tileSize/2 pour que (position.x, position.y) soit le centre du <div>
-						const half = tileSize / 2;
-
-						// Default position if undefined
-						const posX = player.position?.x ?? 0;
-						const posY = player.position?.y ?? 0;
-
-						const baseStyle = {
-							top: posY - half,
-							left: posX - half,
-							width: tileSize,
-							height: tileSize,
-							'--ghost-color': ghostColors[player.character] || 'white'
-						} as React.CSSProperties;
-
-						// Style spécifique pour Pac-Man (rotation)
-
-						return (
-							<div
+							? 'player pacman'
+							: `player ghost ghost-${player.character}`;
+							
+							// On soustrait tileSize/2 pour que (position.x, position.y) soit le centre du <div>
+							const half = tileSize / 2;
+							
+							// Default position if undefined
+							const posX = player.position?.x ?? 0;
+							const posY = player.position?.y ?? 0;
+							
+							const baseStyle = {
+								top: posY - half,
+								left: posX - half,
+								width: tileSize,
+								height: tileSize,
+								'--ghost-color': ghostColors[player.character] || 'white'
+							} as React.CSSProperties;
+							
+							// Style spécifique pour Pac-Man (rotation)
+							
+							return (
+								<div
 								key={player.id}
 								className={className}
 								style={{...baseStyle}}
 								title={`${player.username} (${player.score} pts)`}
-							>
-								{player.character !== 'P' && (
-									<>
-									<div className="ghost-body">
-									<svg viewBox="0 0 56 56" preserveAspectRatio="xMidYMid meet">
-										<polygon points="0 24, 4 24, 4 12, 8 12, 8 8, 12 8, 12 4, 20 4, 20 0, 36 0, 36 4, 44 4, 44 8, 48 8, 48 12, 52 12, 52 24, 56 24, 56 48, 0 48" />
-									</svg>
-
-									</div>
-									<div className="ghost-eye-left">
-										<svg preserveAspectRatio="xMidYMid meet">
-											<polygon points="4 0, 12 0, 12 4, 16 4, 16 16, 12 16, 12 20, 4 20, 4 16, 0 16, 0 4, 4 4" />
+								>
+									{player.character !== 'P' && (
+										<>
+										<div className="ghost-body">
+										<svg viewBox="0 0 56 56" preserveAspectRatio="xMidYMid meet">
+											<polygon points="0 24, 4 24, 4 12, 8 12, 8 8, 12 8, 12 4, 20 4, 20 0, 36 0, 36 4, 44 4, 44 8, 48 8, 48 12, 52 12, 52 24, 56 24, 56 48, 0 48" />
 										</svg>
-									</div>
-									<div className="ghost-eye-right">
-										<svg preserveAspectRatio="xMidYMid meet">
-											<polygon points="4 0, 12 0, 12 4, 16 4, 16 16, 12 16, 12 20, 4 20, 4 16, 0 16, 0 4, 4 4" />
-										</svg>
-									</div>
 
-									
-									<div className="pupil-left"></div>
-									<div className="pupil-right"></div>
-									</>
-								)}
-								{player.character === 'P' && (
-									<>
-										<div className="pacman-body"></div>
-										{/* <div className="pacman-mouth"></div> */}
-										<div className="pacman-eye"></div>
-									</>
-								)}	
-							</div>
-						);
-					})}
+										</div>
+										<div className="ghost-eye-left">
+											<svg preserveAspectRatio="xMidYMid meet">
+												<polygon points="4 0, 12 0, 12 4, 16 4, 16 16, 12 16, 12 20, 4 20, 4 16, 0 16, 0 4, 4 4" />
+											</svg>
+										</div>
+										<div className="ghost-eye-right">
+											<svg preserveAspectRatio="xMidYMid meet">
+												<polygon points="4 0, 12 0, 12 4, 16 4, 16 16, 12 16, 12 20, 4 20, 4 16, 0 16, 0 4, 4 4" />
+											</svg>
+										</div>
+
+										
+										<div className="pupil-left"></div>
+										<div className="pupil-right"></div>
+										</>
+									)}
+									{player.character === 'P' && (
+										<>
+											<div className="pacman-body"></div>
+											{/* <div className="pacman-mouth"></div> */}
+											<div className="pacman-eye"></div>
+										</>
+									)}	
+								</div>
+							);
+						})}
+					</div>
 				</div>
-			</div>
+				{/* 3. Afficher les scores */}
+				<div className="column-right">
+					{/* <h3 className="score">{players[0]?.username} : {players[0]?.score}</h3>
+					<h3 className="score">{players[1]?.username} : {players[1]?.score}</h3> */}
+					{players.slice(0).map((player, index) => (
+						<h3 key={index} className="score">
+							{player.username} : {player.score}
+						</h3>
+					))}
+				</div>
 		</div>
+				</>
 	);
 };
 
