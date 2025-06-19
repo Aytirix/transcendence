886ab09a2c6082c346eda9cb89f7edd108aef1f3
K1000 <151030382+K1000e@users.noreply.github.com>
2025-06-19 13:36:00 +0200

    Ajout de traductions pour le menu Pacman et mise à jour des règles de jeu avec i18next

--- srcs/container/frontend/srcs/src/app/Pacman/menu/hooks/useGameRules.ts
+++ srcs/container/frontend/srcs/src/app/Pacman/menu/hooks/useGameRules.ts
@@ -1,15 +1,18 @@
 import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export function useGameRules() {
+	const { t } = useTranslation();
+
 	const rules = useMemo(() => [
-		"Le jeu se joue entre 2 à 5 joueurs.",
-		"Le jeu se joue en 3 manches de 5 minutes.",
-		"Chaque joueur doit choisir un personnage.",
-		"Le but du jeu est de marquer le plus de points possible.",
-		"Les joueurs peuvent utiliser des power-ups pour gagner un avantage.",
-		"Les joueurs peuvent se battre entre eux pour obtenir des bonus.",
-		"Le joueur avec le plus de points à la fin des 3 manches gagne."
-	], []);
+		t("Le jeu se joue entre 2 à 5 joueurs."),
+		t("Le jeu se joue en 3 manches de 5 minutes."),
+		t("Chaque joueur doit choisir un personnage."),
+		t("Le but du jeu est de marquer le plus de points possible."),
+		t("Les joueurs peuvent utiliser des power-ups pour gagner un avantage."),
+		t("Les joueurs peuvent se battre entre eux pour obtenir des bonus."),
+		t("Le joueur avec le plus de points à la fin des 3 manches gagne.")
+	], [t]);
 
 	return { rules };
 }
