784935c4d4cf560863432daf7924aaccf73857ae
Théo <aytirix@hotmail.com>
2025-06-20 07:35:43 +0200

    feat: Ajout de la récupération des statistiques de jeu Pacman pour l'utilisateur, incluant les scores et les classements

--- srcs/container/backend/srcs/src/routes/pacmanRoutes.ts
+++ srcs/container/backend/srcs/src/routes/pacmanRoutes.ts
@@ -1,6 +1,11 @@
 import { FastifyInstance } from 'fastify';
 import pacmanSchema from '@schemas/pacmanSchema';
 import pacmanController from '@controllers/controllerPacman';
+import Middleware from '@Middleware';
 
 export default async (fastify: FastifyInstance) => {
+	fastify.get('/pacman/statistics', {
+		preHandler: [Middleware.isAuthenticated],
+		schema: pacmanSchema.getStatisticsForUser,
+	}, pacmanController.getStatisticsForUser);
 };
