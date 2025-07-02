12dcbebfebfa0ad4bfa9f6eabe73616c4939f41c
irongab06 <gabrielcavalier@msn.com>
2025-07-02 10:21:55 +0200

    mise a jour du front en multiinvite et fixe bug sur tournament

--- srcs/container/frontend/srcs/src/app/pongGame/modes/MultiPlayersInvite.tsx
+++ srcs/container/frontend/srcs/src/app/pongGame/modes/MultiPlayersInvite.tsx
@@ -5,6 +5,8 @@ import { Engine, Scene, Mesh, AbstractMesh, FreeCamera } from '@babylonjs/core';
 import { initBabylon } from '../initBabylon';
 import { Parse } from '../types/data';
 import { handleKeyDown, handleKeyUp } from '../types/handleKey';
+import ApiService from '../../../api/ApiService';
+
 
 const MultiPlayersInvite: React.FC = () => {
 	const navigate = useNavigate();
@@ -39,6 +41,9 @@ const MultiPlayersInvite: React.FC = () => {
 	const [isWinner, setisWinner] = useState(false);
 	const [waitingPlayers, setWaitingPlayers] = useState(false);
 	const [isPause, setIsPause] = useState(false);
+	const messagePause = useRef("Press [ ESP ] for PLAY");
+	const [player1Avatar, setPlayer1Avatar] = useState<string | undefined>(undefined);
+	const [player2Avatar, setPlayer2Avatar] = useState<string | undefined>(undefined);
 
 	const data = searchParams.get('data');
 	const reconnection = localStorage.getItem("reconnection");
@@ -142,6 +147,8 @@ const MultiPlayersInvite: React.FC = () => {
 			if (data.type === "Pause") {
 				console.log(`pause = ${isPause}`)
 				setIsPause(data.value);
+				if (data.message)
+					messagePause.current = data.message;
 			}
 			if (data.type === "assign") {
 				assignPlayer.current = data.value;
@@ -187,6 +194,13 @@ const MultiPlayersInvite: React.FC = () => {
 				socket.close(); //ici
 			}
 			if (data.ball && data.player1 && data.player2) {
+			if (!player1Avatar)
+				setPlayer1Avatar(ApiService.getFile(data.player1.avatar))
+			if (!player2Avatar)
+				setPlayer2Avatar(ApiService.getFile(data.player2.avatar))
+			if (!namePlayer1.current) {
+				namePlayer1.current = (data.player1.userName);
+			}
 				setParsedData(data)
 
 				if (!namePlayer1.current) {
@@ -351,7 +365,7 @@ const MultiPlayersInvite: React.FC = () => {
 				{/* jeu en pause */}
 				{isPause && isReady3d && isCinematic && !isWinner && (
 					<h1 className='Start-go'>
-						[ Pause ]
+						{messagePause.current}
 					</h1>
 				)}
 
@@ -370,9 +384,25 @@ const MultiPlayersInvite: React.FC = () => {
 				)}
 				{/* Dashboard des scores et exit */}
 				{isCinematic && !isWinner && (
-					<>
-						<h1 className="DashBoardp1">{!namePlayer1.current ? "" : `${namePlayer1.current} : Score ${parsedData?.player1.score}`}</h1>
-						<h1 className="DashBoardp2">{!namePlayer2.current ? "" : `${namePlayer2.current} : Score ${parsedData?.player2.score}`}</h1>
+								<>
+									<h1 className="DashBoardp1">{!namePlayer1.current ? "" : `${namePlayer1.current}`}</h1>
+									<h1 className='DashScore1'>{!namePlayer1.current ? "" : `${parsedData?.player1.score}`}</h1>
+										{!namePlayer1.current ? (
+											""
+											) : (
+											<div className='popup-avatar1'>
+												<img src={player1Avatar} alt="Avatar"/>
+											</div>
+											)}
+									<h1 className="DashBoardp2">{!namePlayer2.current ? "" : `${namePlayer2.current}`}</h1>
+									<h1 className='DashScore2'>{!namePlayer2.current ? "" : `${parsedData?.player2.score}`}</h1>
+										{!namePlayer2.current ? (
+											""
+											) : (
+											<div className='popup-avatar2'>
+												<img src={player2Avatar} alt="Avatar"/>
+											</div>
+											)}
 						<button onClick={returnMenu} className="Return-button">Exit Game</button>
 					</>
 				)}
